4f3a91e4e52f113c9ef26154c1ed4deb96761da7
Yeison <misookim.bts@gmail.com>
2015-10-12 09:53:58 -0500

    Create Load File_datos792.cpp

---
+++ Load File_datos792.cpp
@@ -0,0 +1,27 @@
+/* Load File Sexo Peso Duracion de Embarazo
+*   BTS g
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+main(){
+    FILE *p;
+    int n,i;
+    char sexo[20];
+    float peso, dure;
+    char nom[20];
+    printf("Ingrese nombre del archivo \n");
+    gets(nom);
+    printf("\n tamaño del vector \n");
+    scanf("%d",&n);
+    p=fopen(nom,"r");
+    printf("\n recuperando datos: %d \n", n);
+    for(i = 1; i <= n; i++){
+        fscanf(p, "%s %f %f", &sexo ,&peso, &dure);
+        printf("  %s %3.2f %3.2f \n", sexo, peso, dure);
+    }
+    printf("\n recuperado");
+    fclose(p);
+    return 0;
+}
